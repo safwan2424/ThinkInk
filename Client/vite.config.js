abd198f370454721844db21832cd4d577a122301
Safwan <Safwanjaved24@gmail.com>
2024-12-14 15:48:21 +0530

    Eight 1

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000', // Backend server URL
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix before forwarding
+//       },
+//     },
+//   },
+// });
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,5 +24,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix before forwarding
       },
     },
+  },
+  build: {
+    outDir: 'build', // Specify the output directory as "build"
   },
 });
